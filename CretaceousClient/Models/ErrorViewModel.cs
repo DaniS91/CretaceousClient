556a09f000e7e0f36f2f5f3ee1f66a28672aee3c
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-04-16 09:54:26 -0700

    enable nullable context for error view model

---
+++ CretaceousClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CretaceousClient.Models;
+#nullable enable
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
